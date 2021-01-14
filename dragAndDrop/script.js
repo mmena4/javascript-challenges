a26525f63c2fec19a5dd776017a5c074929235c7
Moi <mmena4@hotmail.com>
2021-01-15 00:19:07 +0100

    Drag and Drop + Index added

--- dragAndDrop/script.js
+++ dragAndDrop/script.js
@@ -1,48 +1,41 @@
-const fill = document.querySelector('')
-const empties = document.querySelectorAll('')
+/*******FILLS*****/
+const fill = document.querySelector(".fill");
+const empties = document.querySelectorAll(".empty");
 
-fill.addEventListener('dragstart', )
-fill.addEventListener('dragend', )
+fill.addEventListener("dragstart", dragStart);
+fill.addEventListener("dragend", dragEnd);
 
-for(const empty of empties) {
-
+function dragStart() {
+  this.className += " flyingAlien";
+  setTimeout(() => (this.className = " invisible"), 0);
 }
 
-
-function allowDrop(event) {
-    event.preventDefault();
-  }
-  
-function dragStart(event) {
-    event.dataTransfer.setData('div', fill);
-    setTimeout()
-}
-
-function drop(event) {
-    event.preventDefault();
-    var data = event.dataTransfer.getData('div');
-    event.target.appendChild(document.getElementsByClassName(empty));
-  }
-
-/*
 function dragEnd() {
-    this.
+  this.className = "fill";
+}
+
+/*****EMPTIES**** */
+for (const empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
 }
 
 function dragOver(e) {
-
+  e.preventDefault();
 }
 
 function dragEnter(e) {
-
+  e.preventDefault();
+  this.className += " hovered";
 }
 
 function dragLeave() {
-
+  this.className = " empty";
 }
 
 function dragDrop() {
-
+  this.className = "empty";
+  this.append(fill);
 }
-
-*/
